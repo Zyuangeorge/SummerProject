077e8ffd274348d20eeb8f6a2d9b9e54dabae4f7
Zyuangeorge <georgeyz365@gmail.com>
2022-06-23 11:55:51 +0100

    dash line complete

--- py_file/read_data.py
+++ py_file/read_data.py
@@ -1,24 +1,43 @@
+import plotly
 import pandas as pd
-import chart_studio.plotly as py
 import plotly.graph_objs as go
 
-df = pd.read_csv('Cycle_Data/CHAM_26/CHAM_26_1_cycles_200.csv', header=8, usecols=['Cycle', 'AH-OUT'])
+data1 = pd.read_csv('Cycle_Data/CHAM_26/CHAM_26_1_cycles_200.csv', header=8)
+data2 = pd.read_csv('Cycle_Data/CHAM_26/CHAM_26_2_cycles_200.csv', header=8)
 
-trace1 = go.Scatter(
-    x = df['Cycle'],
-    y = df['AH-OUT'],
-    mode = "lines",
-    name = "CHAM_26_1_cycles_200",
-    marker = dict(color = 'rgba(16,112,2,0.8)'),
-    text = 'ZW_Cham200cycles_1C.000' )
+""" print(data1.head())
+print(data2.head()) """
 
-data = [trace1]
+line1 = go.Scatter(
+    x=data1['Cycle'], 
+    y=data1['AH-OUT'], 
+    name='CHAM_26_1', 
+    marker_color='rgb(255,0,0)')
 
-layout = dict(title = 'CHAM_26 data',
+line2 = go.Scatter(
+    x=data2['Cycle'], 
+    y=data2['AH-OUT'], 
+    name='CHAM_26_2', 
+    marker_color='rgb(255,0,0)', 
+    line = dict(dash='dash'))
+
+fig = go.Figure([line1,line2])
+
+fig.update_layout(
+    title = 'CHAM_26_cycles_200 Battery 1 and Battery 2 Data',
+    xaxis_title = 'Cycle',
+    yaxis_title = 'AH-OUT'
+)
+
+fig.show()
+
+""" data = [line] """
+
+""" layout = dict(title = 'CHAM_26 data',
             xaxis= dict(title= 'Cycle'),
             yaxis= dict(title= 'AH-OUT'),
-            legend= dict(x=1.1,y=1))
+            legend= dict(x=1.1,y=1)) """
 
-fig = dict(data = data, layout = layout)
+""" fig = dict(data = data, layout = layout) """
 
-py.iplot(fig, filename='AH-OUT/Cycle')
+""" py.iplot(fig, filename='AH-OUT/Cycle') """
