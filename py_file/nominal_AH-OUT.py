4e098cbb168a20753c3934de796badcb03726dda
Zyuangeorge <georgeyz365@gmail.com>
2022-06-27 13:44:52 +0100

    update generalised codes

--- py_file/nominal_AH-OUT.py
+++ py_file/nominal_AH-OUT.py
@@ -1,72 +1,110 @@
 import os
+import functools
+import operator
 import pandas as pd
 import plotly.graph_objects as go
 
-# ====================CHAM_26====================
-# Set the path and path list of the folder
-CHAM_26_path = 'Cycle_Data/CHAM_26/'
-files = os.listdir(CHAM_26_path)
+def create_lines(file_path):
+    '''Plot the nominal curve, file structure should be folder-scv'''
+    # File lists
+    battery1_file_list = []
+    battery2_file_list = []
 
-# Filter out other files
-CHAM_26_1_files = list(
-    filter(lambda x: (x[0:9] == 'CHAM_26_1' and x[-4:] == '.csv'), files))
-CHAM_26_2_files = list(
-    filter(lambda x: (x[0:9] == 'CHAM_26_2' and x[-4:] == '.csv'), files))
+    battery1_file_sorted = []
+    battery2_file_sorted = []
+    
+    line_list = []
+    
+    line_name = file_path[11:18]
 
-# Read the file to file list and sort the list based on the time information
-CHAM_26_1_list = []
-for file in CHAM_26_1_files:
-    tmp = pd.read_csv(CHAM_26_path + file, header=8)
-    CHAM_26_1_list.append(tmp)
-CHAM_26_1_list = sorted(CHAM_26_1_list, key=lambda k: k['Date'][0])
+    # Set the file list of the folder
+    folder_name = file_path
 
-CHAM_26_2_list = []
-for file in CHAM_26_2_files:
-    tmp = pd.read_csv(CHAM_26_path + file, header=8)
-    CHAM_26_2_list.append(tmp)
-CHAM_26_2_list = sorted(CHAM_26_2_list, key=lambda k: k['Date'][0])
+    file_list = os.listdir(file_path)
 
-# Merge the data in the list to one dataframe variable and create a new series called AH-OUT-NOMINAL
-dataset_CHAM_26_1 = pd.concat(
-    CHAM_26_1_list, axis=0, join='outer', ignore_index=True)
-for data in range(len(dataset_CHAM_26_1)):
-    dataset_CHAM_26_1.loc[data, 'Cycle'] = data
+    # Filter out other files
+    battery1_file_list = list(
+        filter(lambda x: (x[7:10] == '_1_' and x[-4:] == '.csv'), file_list))
+    battery2_file_list = list(
+        filter(lambda x: (x[7:10] == '_2_' and x[-4:] == '.csv'), file_list))
+    
+    # Read the file in the file list and sort the list based on the time information
+    for file in battery1_file_list:
+        tmp = pd.read_csv(folder_name + file, header=8)
+        battery1_file_sorted.append(tmp)
 
-dataset_CHAM_26_1['AH-OUT-NOMINAL'] = dataset_CHAM_26_1.apply(
-    lambda x: x['AH-OUT'] / dataset_CHAM_26_1.loc[0, 'AH-OUT'], axis=1)
+    for file in battery2_file_list:
+        tmp = pd.read_csv(folder_name + file, header=8)
+        battery2_file_sorted.append(tmp)
 
-dataset_CHAM_26_2 = pd.concat(
-    CHAM_26_2_list, axis=0, join='outer', ignore_index=True)
-for data in range(len(dataset_CHAM_26_2)):
-    dataset_CHAM_26_2.loc[data, 'Cycle'] = data
+    battery1_file_sorted = sorted(battery1_file_sorted, key=lambda k: k['Date'][0])
+    battery2_file_sorted = sorted(battery2_file_sorted, key=lambda k: k['Date'][0])
 
-dataset_CHAM_26_2['AH-OUT-NOMINAL'] = dataset_CHAM_26_2.apply(
-    lambda x: x['AH-OUT'] / dataset_CHAM_26_2.loc[0, 'AH-OUT'], axis=1)
+    # Merge the data in the list to one dataframe variable
+    dataset_battery1 = pd.concat(
+        battery1_file_sorted, axis=0, join='outer', ignore_index=True)
 
-# Plot the curves
-line_CHAM_26_1 = go.Scatter(x=dataset_CHAM_26_1['Cycle'],
-                            y=dataset_CHAM_26_1['AH-OUT-NOMINAL'],
-                            name='CHAM_26_1',
-                            marker_color='rgb(255,0,0)')
+    dataset_battery2 = pd.concat(
+        battery2_file_sorted, axis=0, join='outer', ignore_index=True)
 
-line_CHAM_26_2 = go.Scatter(x=dataset_CHAM_26_2['Cycle'],
-                            y=dataset_CHAM_26_2['AH-OUT-NOMINAL'],
-                            name='CHAM_26_2',
-                            marker_color='rgb(255,0,0)',
-                            line=dict(dash='dash'))
+    # Reset the cycle values
+    for data in range(len(dataset_battery1)):
+        dataset_battery1.loc[data, 'Cycle'] = data
+    for data in range(len(dataset_battery2)):
+        dataset_battery2.loc[data, 'Cycle'] = data
+    
+    # Create a new series called AH-OUT-NOMINAL
+    dataset_battery1['AH-OUT-NOMINAL'] = dataset_battery1.apply(
+        lambda x: x['AH-OUT'] / dataset_battery1.loc[0, 'AH-OUT'], axis=1)
 
+    dataset_battery2['AH-OUT-NOMINAL'] = dataset_battery2.apply(
+        lambda x: x['AH-OUT'] / dataset_battery2.loc[0, 'AH-OUT'], axis=1)
 
+    # Plot the curves
+    line_battery1 = go.Scatter(x=dataset_battery1['Cycle'],
+                                y=dataset_battery1['AH-OUT-NOMINAL'],
+                                name= line_name + '_1',
+                                marker_color='rgb(255,0,0)')
+
+    line_battery2 = go.Scatter(x=dataset_battery2['Cycle'],
+                                y=dataset_battery2['AH-OUT-NOMINAL'],
+                                name= line_name + '_2',
+                                marker_color='rgb(255,0,0)',
+                                line=dict(dash='dash'))
+
+    line_list = [line_battery1, line_battery2]
+    
+    return line_list
+
+def plot_curves(root_path):
+    folder_list = []
+    line_list = []
+    lines = []
+    
+    folder_list = os.listdir(root_path)
+
+    # folder_list.sort(key=lambda k: k['Date'][0])
+
+    for folder in folder_list:
+        folder_name = root_path + folder + '/'
+        line_list.append(create_lines(folder_name))
+
+    lines = functools.reduce(operator.concat, line_list)
+
+    fig = go.Figure(lines)
+
+    fig.update_layout(
+        title='Battery AH-OUT-NOMINAL/Cycle Data',
+        xaxis_title='Cycle',
+        yaxis_title='AH-OUT-NOMINAL',
+        hovermode='x'
+    )
+
+    return fig
+    
 # ====================Plot codes====================
+if __name__ == "__main__":
 
-# Combine the curves to one figure
-fig = go.Figure([line_CHAM_26_1,line_CHAM_26_2,])
+    fig = plot_curves('Cycle_Data/')
 
-# Config the layout
-fig.update_layout(
-    title='Battery AH-OUT-NOMINAL/Cycle Data',
-    xaxis_title='Cycle',
-    yaxis_title='AH-OUT-NOMINAL',
-    hovermode='x'
-)
-
-fig.show()
+    fig.show()
