3da35cbf9d91f16846cf1e33ea36a61638ff56e5
Zyuangeorge <georgeyz365@gmail.com>
2022-08-01 16:29:37 +0100

    update internal_resistance

--- py_file/inter_resistance_full_cycle.py
+++ py_file/inter_resistance_full_cycle.py
@@ -1,8 +1,4 @@
 import os
-import functools
-import operator
-import random
-import time
 import datetime
 import pandas as pd
 import plotly.graph_objects as go
@@ -44,39 +40,30 @@ def calculate_internal_resistance(data):
     
     return output_data
 
-def create_lines(file_path):
-    csv_data = pd.read_csv(file_path, header = 1)
+def create_lines(battery_data_set, battery_name):
+    internal_resistance_data_full_cycle = []
 
-    internal_resistance_data = calculate_internal_resistance(csv_data)
+    for data in battery_data_set:
+        internal_resistance_data = calculate_internal_resistance(data)
+        internal_resistance_data_full_cycle.append(internal_resistance_data)
 
-    line_1 = go.Scatter(x=internal_resistance_data['Cyc#'],
-                        y=internal_resistance_data['Internal_resistance_1'],
-                        name='Internal resistance_1')
+    internal_resistance_data_full_cycle = pd.concat(
+    internal_resistance_data_full_cycle, axis=0, join='outer', ignore_index=True)
 
-    line_2 = go.Scatter(x=internal_resistance_data['Cyc#'],
-                        y=internal_resistance_data['Internal_resistance_2'],
-                        name='Internal resistance_2')
+    # Reset Cyc# value
+    internal_resistance_data_full_cycle['Cyc#'] = list(internal_resistance_data_full_cycle.index)
 
-    return line_1,line_2
+    line_1 = go.Scatter(x=internal_resistance_data_full_cycle['Cyc#'],
+                        y=internal_resistance_data_full_cycle['Internal_resistance_1'],
+                        name=battery_name + '_' + 'Internal resistance_1')
 
-def plot_graph(root_path):
-    path = root_path
+    line_2 = go.Scatter(x=internal_resistance_data_full_cycle['Cyc#'],
+                        y=internal_resistance_data_full_cycle['Internal_resistance_2'],
+                        name=battery_name + '_' + 'Internal resistance_2')
 
-    line_1, line_2 = create_lines(path)
-
-    fig = go.Figure([line_1,line_2])
-
-    fig.update_layout(
-        title='Battery Internal resistance/Cycle Data',
-        xaxis_title='Cycle',
-        yaxis_title='Internal resistance (R)',
-        hovermode='x' 
-    )
-    
-    return fig
+    return line_1, line_2
 
 def convert_to_timestamp(dateStr):
-    # 12/09/2020 17:51:05
     return datetime.datetime.strptime(dateStr, "%m/%d/%Y %H:%M:%S").timestamp()
 
 def create_dataset(folder_path):
@@ -93,6 +80,7 @@ def create_dataset(folder_path):
     battery_2_file_namelist = list(
         filter(lambda x: (x[7:10] == '_2_' and x[-4:] == '.csv'), file_list))
     
+    # Read the file in the file list
     for file_name in battery_1_file_namelist:
         tmp = pd.read_csv(folder_name + file_name, header=2)
         battery_1_data.append(tmp)
@@ -101,13 +89,35 @@ def create_dataset(folder_path):
         tmp = pd.read_csv(folder_name + file_name, header=2)
         battery_2_data.append(tmp)
 
+    # Sort the list based on the time information
     battery_1_data = sorted(battery_1_data, key = lambda date:convert_to_timestamp(date['DPt Time'][0]))
+    battery_2_data = sorted(battery_2_data, key = lambda date:convert_to_timestamp(date['DPt Time'][0]))
+
+    return battery_1_data, battery_2_data
+
+def plot_graph(folder_path):
+    folder_name = folder_path
+
+    battery_data_1, battery_data_2 = create_dataset(folder_name)
+
+    battery_1_line_1, battery_1_line_2 = create_lines(battery_data_1,folder_name[15:22])
+    battery_2_line_1, battery_2_line_2 = create_lines(battery_data_2,folder_name[15:22] + '_2')
+
+    fig = go.Figure([battery_1_line_1, battery_1_line_2,battery_2_line_1,battery_2_line_2])
+
+    fig.update_layout(
+        title='Battery Internal resistance/Cycle Data',
+        xaxis_title='Cycle',
+        yaxis_title='Internal resistance (R)',
+        hovermode='x',
+        yaxis_range = [-0.1,0.1]
+    )
+    
+    fig.show()
 
 # ====================MAIN====================
 if __name__ == "__main__":
 
-    # fig = plot_graph('Cycle_Data_2/ZW_Cham200cycles_1C_repeat.013.csv')
-    
-    # fig.show()
-
-    create_dataset('Full_Test_Data/CHAM_26/')
+    #plot_graph('Full_Test_Data/CHAM_26/')
+    plot_graph('Full_Test_Data/MOLI_28/')
+    #plot_graph('Full_Test_Data/MOLI_42/')
